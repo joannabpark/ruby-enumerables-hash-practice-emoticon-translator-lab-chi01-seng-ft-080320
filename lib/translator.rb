5aeb659606926753f2ad5536c46eef40dae9c20a
JoAnna Park <joannabpark1@gmail.com>
2020-07-24 18:52:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file, eng_emoji)
   # code goes here
   emoticons = load_library(file)
   emoticons.each do |meaning, idioms|
-       if idioms[:english] == eng_emo
+       if idioms[:english] == eng_emoji
          return idioms[:japanese]
   end
   "Sorry, that emoticon was not found"
